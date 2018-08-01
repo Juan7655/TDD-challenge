3a02cb8b7dac5d0ffda914148551ef61268e03db
Juan David <david.rodriguez@endava.com>
2018-08-01 11:04:14 -0500

    modified code to run merge-sort algorithm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,24 +9,36 @@ public class Main {
         // write your code here
     }
 
-    static List sort(List<Integer> unordered){
-        List<Integer> ordered = new ArrayList<>();
+    static List sort(List<Integer> unordered) {
+        List<Integer> l1 = unordered.subList(0, unordered.size()/2),
+                l2 = unordered.subList(unordered.size()/2, unordered.size());
 
-        if(unordered.size() == 0) return ordered;
-        ordered.add(unordered.get(0));
+        if(l1.size() == 0) return l2;
 
-        for (int i=1; i < unordered.size(); i++)
-            for (int j = 0; j < ordered.size(); j++) {
-                if (unordered.get(i) <= ordered.get(j)) {
-                    ordered.add(j, unordered.get(i));
-                    break;
-                }
-                if(j == ordered.size()-1){
-                    ordered.add(unordered.get(i));
-                    break;
-                }
+        l1 = sort(l1);
+        l2 = sort(l2);
+
+        return merge(l1, l2);
+    }
+
+    private static List merge(List<Integer> l1, List<Integer> l2) {
+        int pointer1 = 0, pointer2 = 0;
+        List<Integer> orderedList = new ArrayList<>();
+
+        while(true){
+            if(l1.get(pointer1) > l2.get(pointer2))  orderedList.add(l2.get(pointer2++));
+            else orderedList.add(l1.get(pointer1++));
+
+            if(pointer1 == l1.size()){
+                orderedList.addAll(l2.subList(pointer2, l2.size()));
+                break;
             }
+            if(pointer2 == l2.size()){
+                orderedList.addAll(l1.subList(pointer1, l1.size()));
+                break;
+            }
+        }
 
-        return ordered;
+        return orderedList;
     }
 }
